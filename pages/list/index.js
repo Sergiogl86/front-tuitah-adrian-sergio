a962626196b16ed2c507c155ee9acd37ad786b9c
AdrianPaoletti <adriandelapao@gmail.com>
2021-11-17 19:19:50 +0100

    Added comented sort by date function

--- pages/list/index.js
+++ pages/list/index.js
@@ -1,5 +1,11 @@
 import Link from "next/link";
 
+// const SortByDate = posts.sort((a, b) => {
+//   const beforeDate = DateTime.fromFormat(a.frontmatter.date, 'M/d/yyyy')
+//   const afterDate = DateTime.fromFormat(b.frontmatter.date, 'M/d/yyyy')
+//   return afterDate - beforeDate
+// })
+
 const ListTuitah = ({ posts }) => (
   <>
     <h1>List Tuitah</h1>
@@ -14,8 +20,7 @@ const ListTuitah = ({ posts }) => (
           </Link >
         </li>))}
     </ul>
-  </>
-)
+  </>)
 // https://tuitah-sergio-adri.herokuapp.com/
 
 export const getStaticProps = async () => {
