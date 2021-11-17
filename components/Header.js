69a5d1e2e03fe43702469eecce44cd803c2c16c3
AdrianPaoletti <adriandelapao@gmail.com>
2021-11-17 18:21:58 +0100

    Minor changes to header component

--- components/Header.js
+++ components/Header.js
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 const Header = () => (
   <div>
-    <Link href="/list-post">
-      <a> LIST -</a>
+    <Link href="/list">
+      <a> LIST --</a>
     </Link>
-    <Link href="/create-post">
+    <Link href="/create">
       <a> CREATE </a>
     </Link>
   </div>
